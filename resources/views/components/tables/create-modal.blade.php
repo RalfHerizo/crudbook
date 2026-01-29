984f1002a60fd4f75c97453431a4dba6e246f838
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-01-29 12:13:32 +0300

    fix(modal): break down the creation modal of the table

---
+++ resources/views/components/tables/create-modal.blade.php
@@ -0,0 +1,39 @@
+@props(['id'])
+
+<x-modal-layout id="{{ $id }}">
+    <form method="POST" action="{{ route('books.store') }}">
+        @csrf
+        <div class="grid gap-4 mb-4 sm:grid-cols-2">
+            <div>
+                <label for="title"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
+                <input type="text" name="title" id="title"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Ecrire le titre ici..." required="">
+            </div>
+            <div>
+                <label for="author"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Auteur</label>
+                <input type="text" name="author" id="author"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Ecrire le nom de l'auteur ici..." required="">
+            </div>
+            <div class="sm:col-span-2"><label for="description"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                <textarea id="description" rows="4"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    name="description" placeholder="Ecrire la description du livre ici..."></textarea>
+            </div>
+        </div>
+        <button type="submit"
+            class="flex text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary-soft shadow-xs font-medium leading-5 rounded-base text-sm px-2 py-2.5 focus:outline-none">
+            <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewbox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                    clip-rule="evenodd" />
+            </svg>
+            Ajouter un livre
+        </button>
+    </form>
+</x-modal-layout>
