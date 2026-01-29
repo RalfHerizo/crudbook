fdda4e6171391da0f20d60a0702bb2fe1f2b69d9
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-01-29 11:45:58 +0300

    feat(homepage): create landing page

---
+++ resources/views/components/book-illustrations.blade.php
@@ -0,0 +1,48 @@
+<!-- Decorative Leaves -->
+<div class="absolute top-10 left-10 w-16 h-16 leaf">
+    <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M50 10C30 30 20 50 20 70C20 80 25 90 35 95C40 75 50 50 70 30C75 25 80 20 85 20C80 15 65 10 50 10Z" fill="#86efac" opacity="0.8"/>
+    </svg>
+</div>
+
+<div class="absolute top-5 right-20 w-20 h-20 leaf" style="animation-delay: -1s;">
+    <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M50 10C30 30 20 50 20 70C20 80 25 90 35 95C40 75 50 50 70 30C75 25 80 20 85 20C80 15 65 10 50 10Z" fill="#4ade80" opacity="0.9"/>
+    </svg>
+</div>
+
+<!-- Book Stack -->
+<div class="book-stack relative">
+    <!-- Red Book (Top) -->
+    <div class="absolute top-0 left-1/2 transform -translate-x-1/2 w-48 h-36 bg-gradient-to-br from-red-400 to-red-500 rounded-lg shadow-2xl" style="transform: translate(-50%, 0) rotate(-2deg);">
+        <div class="absolute right-2 top-0 bottom-0 w-2 bg-red-600 rounded-r-lg"></div>
+        <div class="absolute inset-0 border-4 border-red-600 rounded-lg"></div>
+    </div>
+    
+    <!-- Yellow Book (Middle) -->
+    <div class="absolute top-16 left-1/2 transform -translate-x-1/2 w-52 h-40 bg-gradient-to-br from-yellow-300 to-yellow-400 rounded-lg shadow-2xl" style="transform: translate(-50%, 0) rotate(1deg);">
+        <div class="absolute right-2 top-0 bottom-0 w-2 bg-yellow-600 rounded-r-lg"></div>
+        <div class="absolute inset-0 border-4 border-yellow-500 rounded-lg"></div>
+    </div>
+    
+    <!-- Orange Book (Bottom) -->
+    <div class="absolute top-32 left-1/2 transform -translate-x-1/2 w-56 h-44 bg-gradient-to-br from-orange-300 to-orange-400 rounded-lg shadow-2xl" style="transform: translate(-50%, 0) rotate(-1deg);">
+        <div class="absolute right-2 top-0 bottom-0 w-2 bg-orange-600 rounded-r-lg"></div>
+        <div class="absolute inset-0 border-4 border-orange-500 rounded-lg"></div>
+    </div>
+    
+    <!-- Decorative Elements -->
+    <!-- Sticky Note -->
+    <div class="absolute bottom-20 -left-12 w-20 h-20 bg-yellow-200 rounded shadow-lg transform rotate-12"></div>
+    
+    <!-- Pencils -->
+    <div class="absolute -bottom-10 right-8 flex gap-2">
+        <div class="w-6 h-32 bg-gradient-to-b from-purple-400 to-purple-500 rounded-full shadow-lg transform rotate-12"></div>
+        <div class="w-6 h-28 bg-gradient-to-b from-blue-400 to-blue-500 rounded-full shadow-lg transform -rotate-6"></div>
+    </div>
+    
+    <!-- Circle decoration -->
+    <div class="absolute -bottom-5 right-16 w-12 h-12 bg-white rounded-full shadow-lg">
+        <div class="absolute inset-2 bg-gradient-to-br from-yellow-300 to-orange-300 rounded-full"></div>
+    </div>
+</div>
