14cf8e92d7ce45b2750da86476a9a527760a08af
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-01-30 16:19:08 +0300

    fix(delete-modal):break down the delete modal component and closed the div tag

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -59,7 +59,8 @@
     <!-- Read modal -->
     <x-tables.preview-modal id="readProductModal" title="Détails du livre" />
     <!-- Delete modal -->
-    <div id="deleteModal" tabindex="-1" aria-hidden="true"
+    <x-tables.delete-modal id="deleteModal" title="Delete item?" />
+    {{-- <div id="deleteModal" tabindex="-1" aria-hidden="true"
         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-md max-h-full">
             <!-- Modal content -->
@@ -94,5 +95,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+    
 @endsection
--- resources/views/components/tables/preview-modal.blade.php
+++ resources/views/components/tables/preview-modal.blade.php
@@ -13,4 +13,5 @@
             <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Description</dt>
             <dd id="read-modal-book-description" class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400"></dd>
         </dl>
+    </div>
 </x-modal-layout>
