fa8b8ae5430b782622950234f8e7d0624723abab
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-02-10 17:26:24 +0300

    test(search): add function test for book search

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -11,7 +11,7 @@ test('welcome_page', function () {
     $response->assertStatus(200);
 });
 
-test('Add book Test', function(){
+test('Add book TEST', function(){
     $book = Book::create([
         'title'=>'Juste un livre',
         'author'=>'test est author',
@@ -26,4 +26,25 @@ test('Add book Test', function(){
     
     $response->assertRedirect(route('books.index'));
     $response->assertSessionHas('success', 'Livre supprimé avec succès!');
+});
+
+test('Looking for a book TEST', function(){
+    Book::create([
+        'title'=> 'Harry Potter',
+        'author'=> 'J.K. Rowling',
+        'description'=> 'Un sorcier célèbre',
+    ]);
+
+    Book::create([
+        'title' => 'Le Hobbit',
+        'author' => 'J.R.R. Tolkien',
+        'description' => 'Une aventure en Terre du Milieu'
+    ]);
+
+    $response = $this->get(route('books.index',['search'=>'Harry']));
+
+    $response->assertStatus(200);
+    $response->assertSee('Harry');
+    $response->assertDontSee('Le Hobbit');
+
 });
